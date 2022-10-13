6e7a1143582d72e97e9353dd9b5dde44442f3bbc
Dumisani <dendumi@gmail.com>
2022-10-13 08:58:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main (void)
+
+/**
+
+     * main - Entry point
+     *
+     *  *
+     *
+     *   * Return: End with the value 0
+     *
+     *    */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
