0dfa2e57e6e3147e4b79672022cbd5522327fb5b
Dumisani <dendumi@gmail.com>
2022-10-14 11:55:51 -0700

    6-Print Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - Print single digit numbers 
- 	using putchar without char viariables.
+ *main - Print single digit numbers using putchar without char viariables.
  *
  * Return:0.
  */
 int main(void)
 {
 	int i = 0;
-
-	while(i < 10)
-	{
+	
+	while (i < 10)
+	{	
 		putchar((i % 10) + '0');
 		i++;
 	}
 	putchar('\n');
-	return (0);
+	return (0)
 }
