19d419a7906da5b934a02dd5a475c80533d3216b
Dumisani <dendumi@gmail.com>
2022-10-15 02:51:59 -0700

    100 Print Combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (num = 0; num < 9; num++)
 	{
-		for (num2 += num; num2 < 10; num2++)
+		for (num2 = num + 1; num2 < 10; num2++)
 		{
 			putchar((num % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num == 8 && num2 == 10)
-				break;
+			if (num == 8 && num2 == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
