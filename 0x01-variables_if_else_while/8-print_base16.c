57def7aa026ff0c6d0d866894511756aaa1a02dc
Dumisani <dendumi@gmail.com>
2022-10-15 02:27:38 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - Prints base 16 numbers.
+ *
+ * Return: zero.
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 1; chr <= 54; chr++)
+	{
+		if (chr <= 9)
+		{
+			putchar(chr + '0');
+		}
+		else if (chr >= 49)
+		{
+			putchar(chr + '0');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
