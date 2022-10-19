fe77876420a8f9764a0583d12e5f325a4008845b
Dumisani <dendumi@gmail.com>
2022-10-19 02:29:09 -0700

    CheckLowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 /**
  *_islower - Checks for lower case character..
  *
- * Return: 1 if c or 0 otherwise
+ *&c: Character to check
+ * Return: 1 if c is lowercase or 0 otherwise
  */
 int _islower(int c)
 {
-	return(c >= 'a' && c <= 'z');
+	return( c >= 'a' && c <= 'z');
 }
