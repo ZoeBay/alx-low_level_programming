8a0f3fba6214cf095c700d39ba6e26a25bedff46
Dumisani <dendumi@gmail.com>
2022-10-20 10:35:15 -0700

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - Checks for uppercase character
+ *@c:character
+ *Return: 1 if c is a digit, otherwise 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
