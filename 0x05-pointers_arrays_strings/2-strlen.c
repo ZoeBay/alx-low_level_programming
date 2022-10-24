26e32bf8aac6536429837e3faa44e739e40b321e
Dumisani <dendumi@gmail.com>
2022-10-24 10:02:29 -0700

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - Returns the length of a string
+ *@s:String
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (*(s + i))
+		i++;
+	return (i);
+}
